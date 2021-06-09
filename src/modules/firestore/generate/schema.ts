a0fc78c801e111fcb9ce8a7543a2921a671958ac
pierrot <drouilletpierre@gmail.com>
2021-06-09 10:21:56 +0200

    v1.12.1

--- src/modules/firestore/generate/schema.ts
+++ src/modules/firestore/generate/schema.ts
@@ -1,34 +1,42 @@
 import { print, DocumentNode, parse } from 'graphql'
 
 import { transformTypeToInput } from '@browserql/inputs'
-import { getDirective, getFields, getKind, getName, getTypes, parseKind } from '@browserql/fpql'
+import {
+  getDirective,
+  getFields,
+  getKind,
+  getName,
+  getTypes,
+  parseKind,
+  getType,
+} from '@browserql/fpql'
 
 import staticSchema from '../static-schema'
 import dynamicSchema from '../dynamic-schema'
-
+import { FirestoreWhereInput } from '../types'
 
 export default function generateSchema(schema: DocumentNode) {
   const types = getTypes(schema as DocumentNode)
-    const models = types.filter(getDirective('firestore'))
-    const defs: string[] = [
-      print(staticSchema)
-    ]
-    models.forEach(model => {
-      const modelName = getName(model)
-      
-      defs.push(
-        print(dynamicSchema).replace(/TYPE/g, modelName),
-        print(transformTypeToInput(model, schema as DocumentNode))
-          .replace(`input ${modelName}Input `, `input ${modelName}FirestoreInput `),
-      )
-      
-      const fields = getFields(model)
-      
-      if (!fields.find(field => getName(field) === 'id')) {
-        defs.push(`extend type ${modelName} { id: ID! }`)
-      }
+  const models = types.filter(getDirective('firestore'))
+  const defs: string[] = [print(staticSchema)]
+  models.forEach((model) => {
+    const modelName = getName(model)
 
-      defs.push(`
+    defs.push(
+      print(dynamicSchema).replace(/TYPE/g, modelName),
+      print(transformTypeToInput(model, schema as DocumentNode)).replace(
+        `input ${modelName}Input `,
+        `input ${modelName}FirestoreInput `
+      )
+    )
+
+    const fields = getFields(model)
+
+    if (!fields.find((field) => getName(field) === 'id')) {
+      defs.push(`extend type ${modelName} { id: ID! }`)
+    }
+
+    defs.push(`
         input FirestoreRef_${modelName} {
           endAfter: ID
           endAt: ID
@@ -40,28 +48,51 @@ export default function generateSchema(schema: DocumentNode) {
         }
 
         input FirestoreWhere_${modelName} {
-          ${fields.map(field => `${getName(field)}: FirestoreWhere_${modelName}_${getName(field)}`)}
+          ${fields.map(
+            (field) =>
+              `${getName(field)}: FirestoreWhere_${modelName}_${getName(field)}`
+          )}
         }
       `)
 
-      defs.push(...fields.map(field => {
-        const { type: kind } = parseKind(getKind(field))
+    defs.push(
+      ...fields.map((field) => {
+        const { type: kind, depth } = parseKind(getKind(field))
+
+        let trueKind = kind
+
+        const obj = getType(kind)(schema)
+
+        if (obj) {
+          trueKind = `${kind}Input`
+        }
+
+        const attrs: Partial<Record<keyof FirestoreWhereInput, string>> = {
+          equals: trueKind,
+          equalsNot: trueKind,
+          in: `[${trueKind}]`,
+          notIn: `[${trueKind}]`,
+          greaterThan: 'Float',
+          greaterThanOrEqual: 'Float',
+          lessThan: 'Float',
+          lessThanOrEqual: 'Float',
+        }
+
+        if (depth) {
+          attrs.arrayContains = trueKind
+          attrs.arrayContainsAny = `[${trueKind}]`
+        }
+
         return `
           input FirestoreWhere_${modelName}_${getName(field)} {
-            arrayContains: ${kind}
-            arrayContainsAny: [${kind}]
-            equals: ${kind}
-            equalsNot: ${kind}
-            greaterThan: Float
-            greaterThanOrEqual: Float
-            in: [${kind}]
-            lessThan: Float
-            lessThanOrEqual: Float
-            notIn: [${kind}]
+            ${Object.keys(attrs)
+              .map((attr) => `${attr}: ${attrs[attr as keyof typeof attrs]}`)
+              .join(`\n${' '.repeat(12)}`)}
           }
         `
-      }))
-    })
-    const finalSchema = parse([...defs].join('\n'))
-    return finalSchema
+      })
+    )
+  })
+  const finalSchema = parse([...defs].join('\n'))
+  return finalSchema
 }
